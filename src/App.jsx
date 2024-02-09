3a79d058154084b30604f07fbb98aa8e22688ec6
Ragib Shahriar <ragibshahriar58@gmail.com>
2024-02-09 21:16:21 +0600

    Doctors Blog and Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
+import Doctors from './components/Doctors'
+import Blogs from './components/Blogs' 
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -21,7 +24,17 @@ const App = () => {
         <div id='services'>
           <Services />
         </div>
+
+        <div id='doctors'>
+          <Doctors />
+        </div>
+
+        <div id='blogs'>
+          <Blogs />
+        </div>
       </main>
+
+      <Footer />
     </>
   )
 }
